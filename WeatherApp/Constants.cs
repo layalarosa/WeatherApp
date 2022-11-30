72aa8ada17204f72ad41b8202076f8f15d57a56d
layalarosa <layalarosa@gmail.com>
2022-11-30 17:28:42 -0400

    Add project files.

---
+++ WeatherApp/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    public class Constants
+    {
+        public static string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public static string OpenWeatherMapAPIKey = "6cfbec15314cf97fa5d556eccf500c54";
+    }
+}
